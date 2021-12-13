fe0e26c2126e90a46f2202e03b9e3d1c21ca875a
조원진 <>
2021-12-13 20:49:52 +0900

    init

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lose Yourself
         </a>
       </header>
     </div>
